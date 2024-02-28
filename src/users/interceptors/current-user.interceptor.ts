76833208714d6fad2616147152bcbb964ed728f5
RaphaelFrey <raphael.marius.frey@gmail.com>
2024-02-28 16:11:25 +0100

    Initial commit

---
+++ src/users/interceptors/current-user.interceptor.ts
@@ -0,0 +1,16 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { UsersService } from "../users.service";
+
+@Injectable()
+export class CurrentUserInterceptor implements NestInterceptor {
+  constructor(private readonly usersService: UsersService) {
+  }
+  async intercept(context: ExecutionContext, next: CallHandler<any>) {
+    const request = context.switchToHttp().getRequest();
+    const { userId } = request.session || {};
+    if (userId) {
+      request.currentUser = await this.usersService.findOne(userId);
+    }
+    return next.handle();
+  }
+}
